1f96e4b8b1662377d4ccb0e5c1fdd2ecf1c5082b
Sam <thesamreaves@gmail.com>
2019-01-26 11:08:39 -0500

    Initial Commit

---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Sam Reaves on 1/26/19.
+//  Copyright © 2019 Sam Reaves Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
