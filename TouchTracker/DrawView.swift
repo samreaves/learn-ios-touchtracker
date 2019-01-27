b00e96f60b1ceee96ecc2d3fefd428aa7eb0ac23
Sam <thesamreaves@gmail.com>
2019-01-26 22:31:54 -0500

    Added @IBInspectable to line thickness, line colors. Very cool feature

--- TouchTracker/DrawView.swift
+++ TouchTracker/DrawView.swift
@@ -12,9 +12,27 @@ class DrawView: UIView {
     var currentLines = [NSValue: Line]()
     var finishedLines = [Line]()
     
+    @IBInspectable var finishedLineColor: UIColor = UIColor.black {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var currentLinesColor: UIColor = UIColor.red {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var lineThickness: CGFloat = 10 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
     func stroke(_ line: Line) {
         let path = UIBezierPath()
-        path.lineWidth = 10
+        path.lineWidth = lineThickness
         path.lineCapStyle = .round
         
         path.move(to: line.begin)
@@ -23,12 +41,12 @@ class DrawView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
-        UIColor.black.setStroke()
+        finishedLineColor.setStroke()
         for line in finishedLines {
             stroke(line)
         }
         
-        UIColor.red.setStroke()
+        currentLinesColor.setStroke()
         for (_, line) in currentLines {
             stroke(line)
         }
